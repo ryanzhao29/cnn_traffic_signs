3abd7eddc9e3b80989f3cfe5f4bf302977861d7d
ryanzhao29 <ryanzhao29@gmail.com>
2017-01-29 21:54:59 -0500

    added dropout to the 2nd fc layer
    added dropout to the 2nd fc layer

--- define_leNet.py
+++ define_leNet.py
@@ -17,13 +17,15 @@ class def_leNet():
         self.num_fc_layer2 = 84
         self.num_fc_layer3 = classification_num
 
-    def get_training_model(self, x):
+    def get_training_model(self, x, enable_dropout = True):
         conv1, weight1 = new_convolution_layer(x, self.color_channel, self.filter_size_conv_layer1, self.filter_size_conv_layer1, self.num_filter_conv_layer1, True, 'SAME')
         conv2, weight2 = new_convolution_layer(conv1, self.num_filter_conv_layer1, self.filter_size_conv_layer2, self.filter_size_conv_layer2, self.num_filter_conv_layer2, True, 'SAME')
         fltten_layer, num_fueatures = flatten_layer(conv2, self.num_filter_conv_layer2, self.image_size)
-        fc1,weight_fc1,bias_fc1 = new_fc_layer(fltten_layer,num_fueatures, self.num_fc_layer1, use_relu = True)
-        fc2,weight_fc2,bias_fc2 = new_fc_layer(fc1,self.num_fc_layer1, self.num_fc_layer2, use_relu = True)
-        fc3,weight_fc3,bias_fc3 = new_fc_layer(fc2,self.num_fc_layer2, self.num_fc_layer3, use_relu = False)
+        fc1,weight_fc1,bias_fc1 = new_fc_layer(fltten_layer, num_fueatures, self.num_fc_layer1, use_relu = True)
+        fc2,weight_fc2,bias_fc2 = new_fc_layer(fc1, self.num_fc_layer1, self.num_fc_layer2, use_relu = True)
+        if enable_dropout == True:
+            fc2 = tf.nn.dropout(fc2, keep_prob = dropout_keep_prob)
+        fc3,weight_fc3,bias_fc3 = new_fc_layer(fc2, self.num_fc_layer2, self.num_fc_layer3, use_relu = False)
         return fc3
 
 
--- global_head_file.py
+++ global_head_file.py
@@ -2,5 +2,6 @@ import tensorflow as tf
 image_size = 32
 classification_num = 43
 color_channels = 3
+dropout_keep_prob = 0.5
 x = tf.placeholder(tf.float32, shape = [None, None, None, color_channels])
 y = tf.placeholder(tf.float32, shape = [None, classification_num])
